a729c4295699acaa342fd9eaf536c2a98eb8ef0d
Michael Bernasol <mfbernasol@gmail>
2019-10-27 18:41:43 -0500

    Initial Commit

---
+++ Quick Chat/LogInViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LogInViewController.swift
+
+//
+// where users login
+
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class LogInViewController: UIViewController {
+
+
+    @IBOutlet var emailTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+   
+    //Log in an existing user
+    
+    @IBAction func logInPressed(_ sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Log in successful!")
+                
+                SVProgressHUD.dismiss()
+                
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+                
+            }
+            
+        }
+        
+    }
+    
+
+
+    
+}  
